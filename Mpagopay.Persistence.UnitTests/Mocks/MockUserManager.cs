609f1fce219771a61fe7c0ec1f5071ae8a887d61
Hervé TEGUIA <tkh.tegus@gmail.com>
2023-06-09 19:45:52 +0400

    up

--- Mpagopay.Persistence.UnitTests/Mocks/MockUserManager.cs
+++ Mpagopay.Persistence.UnitTests/Mocks/MockUserManager.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Claims;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Security.Claims;
 using Microsoft.AspNetCore.Identity;
 using Moq;
-using Mpagopay.Application.Models.Mail;
 using Mpagopay.Identity.Models;
 
 namespace Mpagopay.Persistence.UnitTests.Mocks
@@ -15,13 +9,48 @@ namespace Mpagopay.Persistence.UnitTests.Mocks
     {
         public static Mock<UserManager<ApplicationUser>> GetUserManager() 
         {
+            ApplicationUser applicationUserOne = new() 
+            { 
+                Id = "d0045786-628a-4e7a-ba37-a3144071dac4", 
+                Email = "cotl@gmail.com", 
+                UserName = "cotl", 
+                PasswordHash = "AQAAAAEAACcQAAAAEBAe9K82p1cp7CDfojjQoXIOKy/sBOXZY29JwMPInAt1/gSJXvUgk7Yx7W7TyUSIZA==",
+                EmailConfirmed = true 
+            };
+            ApplicationUser applicationUserTwo = new()
+            {
+                Id = "7edbf98d-4562-45c1-aa9b-b29c1052e8b0",
+                Email = "john@gmail.com",
+                UserName = "john",
+                PasswordHash = "AQAAAAEAACcQAAAAEBAe9K82p1cp7CDfojjQoXIOKy/sBOXZY29JwMPInAt1/gSJXvUgk7Yx7W7TyUSIZA==",
+                EmailConfirmed = true
+            };
+            ApplicationUser applicationUserThree = new() 
+            { 
+                Id = "999f7b03-5c3c-48d4-84b8-1d397c6143a3", 
+                Email = "timon@gmail.com", 
+                UserName = "timon",
+                PasswordHash = "AQAAAAEAACcQAAAAEBAe9K82p1cp7CDfojjQoXIOKy/sBOXZY29JwMPInAt1/gSJXvUgk7Yx7W7TyUSIZA==",
+                EmailConfirmed = true 
+            };
+            ApplicationUser applicationUserFour = new() { 
+                Id = "0634dae9-166f-460f-85d9-d1cd7345ec1b", 
+                Email = "steven@gmail.com", 
+                UserName = "steven",
+                PasswordHash = "AQAAAAEAACcQAAAAEBAe9K82p1cp7CDfojjQoXIOKy/sBOXZY29JwMPInAt1/gSJXvUgk7Yx7W7TyUSIZA==",
+                EmailConfirmed = true 
+            };
+            ApplicationUser applicationUserFive = new() { 
+                Id = "d3971174-1ace-48a5-8d43-3537e40077a2", 
+                Email = "omar@gmail.com", 
+                UserName = "omar",
+                PasswordHash = "AQAAAAEAACcQAAAAEBAe9K82p1cp7CDfojjQoXIOKy/sBOXZY29JwMPInAt1/gSJXvUgk7Yx7W7TyUSIZA==",
+                EmailConfirmed = true ,
+            };
+            
             List<ApplicationUser> _users = new()
             {
-                new ApplicationUser{ Id = Guid.NewGuid().ToString(), Email = "cotl@gmail.com", UserName = "cotl", EmailConfirmed = true },
-                new ApplicationUser{ Id = Guid.NewGuid().ToString(), Email = "john@gmail.com", UserName = "john", EmailConfirmed = true },
-                new ApplicationUser{ Id = Guid.NewGuid().ToString(), Email = "timon@gmail.com", UserName = "timon", EmailConfirmed = true },
-                new ApplicationUser{ Id = Guid.NewGuid().ToString(), Email = "steven@gmail.com", UserName = "steven", EmailConfirmed = true },
-                new ApplicationUser{ Id = Guid.NewGuid().ToString(), Email = "omar@gmail.com", UserName = "omar", EmailConfirmed = true }
+                applicationUserOne, applicationUserTwo, applicationUserThree, applicationUserFour, applicationUserFive
             };
 
             var store = new Mock<IUserStore<ApplicationUser>>();
@@ -34,8 +63,11 @@ namespace Mpagopay.Persistence.UnitTests.Mocks
             mgr.Setup(x => x.UpdateAsync(It.IsAny<ApplicationUser>())).ReturnsAsync(IdentityResult.Success);
             mgr.Setup(x => x.GetClaimsAsync(It.IsAny<ApplicationUser>())).Returns(Task.FromResult(new List<Claim>() as IList<Claim>));
             mgr.Setup(x => x.GetRolesAsync(It.IsAny<ApplicationUser>())).Returns(Task.FromResult(new List<string>() as IList<string>));
-            mgr.Setup(x => x.FindByEmailAsync("john@gmail.com")).ReturnsAsync(new ApplicationUser { Email = "john@gmail.com", UserName = "john" });
+            mgr.Setup(x => x.FindByEmailAsync("john@gmail.com")).ReturnsAsync(applicationUserTwo);
             mgr.Setup(x => x.FindByEmailAsync("noexist@gmail.com")).ReturnsAsync(null as ApplicationUser);
+            mgr.Setup(x => x.GeneratePasswordResetTokenAsync(It.IsAny<ApplicationUser>())).ReturnsAsync("token");
+            mgr.Setup(x => x.FindByIdAsync("7edbf98d-4562-45c1-aa9b-b29c1052e8b0")).ReturnsAsync(applicationUserTwo);
+            mgr.Setup(x => x.ChangePasswordAsync(applicationUserTwo, "dp@5y!gW", It.IsAny<string>())).ReturnsAsync(IdentityResult.Success);
             mgr.Setup(x => x.Users).Returns(_users.AsQueryable());
             
 
